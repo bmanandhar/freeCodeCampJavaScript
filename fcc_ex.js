9f9c551e6d02bd66e6420a0d9b1f600e38246bff
bmanandhar <bijaya.manandhar@hotmail.com>
2018-04-11 17:22:26 -0700

    more examples of methods

--- fcc_ex.js
+++ fcc_ex.js
@@ -473,7 +473,115 @@ function testSize(num) {
 }
 console.log(testSize(7));
 //
-
+function golfScore(par, strokes) {
+    if (strokes === 1) {
+        return "Hole-in-one";
+    }
+    else if (strokes <= par - 2) {
+        return "Eagle";
+    }
+    else if (strokes === par - 1) {
+        return "Birdie";
+    }
+    else if (strokes === par) {
+        return "Par";
+    }
+    else if (strokes === par + 1) {
+        return "Bogey";
+    }
+    else if (strokes === par + 2) {
+        return "Double Bogey";
+    }
+    else if (strokes <= par + 3) {
+        return "Go Home!";
+    }
+}
+console.log(golfScore(5, 4));
+//
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+    return answer;
+}
+console.log(caseInSwitch(4));
+//
+function switchOfStuff(val) {
+    var answer = "";
+    switch(val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+    }
+    return answer;
+}
+console.log(switchOfStuff("b"));
+//
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    }
+    return answer;
+}
+console.log(sequentialSizes(5));
+//
+function chainToSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42:
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    return answer;
+}
+console.log(chainToSwitch(7));
 
 
 
